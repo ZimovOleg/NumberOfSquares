bc5db95d9deb520f0188e639c0abe3dc87dc4821
ZimovOleg <zimov.oleg@gmail.com>
2022-01-17 12:38:15 +0400

    src/main/java/ru/netology/sqr/SQRService.java

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,18 +1,15 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int calculate(int min, int max) {
-
-        //int min = 200;
-        //int max = 300;
-        int calc = 0;
+    public int NumberOfSquares(int min, int max) {
+//calculate
+        int number = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= min && i * i <= max) {
-                calc = calc + 1;
+                number++;
             }
-
         }
-        return calc;
+        return number;
 
 
     }
